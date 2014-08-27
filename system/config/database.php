b6339e1a8164a8d3aafa4a54c457bd3b38376da4
liuyuxiao <liuyuxiao123@gmail.com>
2014-08-27 13:07:22 +0800

    wecenter init

---
+++ system/config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => '19900410lyx',
+  'dbname' => 'mysql',
+  'port' => '8889',
+);
+$config['slave'] = false;
+$config['port'] = '8889';
